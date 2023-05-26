33430ab546c2f34045e62ee2866690621fb460af
Harish Soni <harish.soni@coditas.com>
2023-05-26 15:00:35 +0530

    Fixed small issues

--- src/components/Featured/index.tsx
+++ src/components/Featured/index.tsx
@@ -1,92 +1,43 @@
 import React from "react";
 import { IBlog } from "../../interfaces";
-import { formatDate } from "../../utils/generalUtils";
 import { BlogCard } from "../reusableComponents/BlogCard";
-import { Image } from "../reusableComponents/Image";
 import "./featured.scss";
+import { HeroSection } from "coditas-ui";
+import { BASE_URL } from "../../HTTP";
 
 interface IFeaturedProps {
   featuredBlog?: IBlog;
-  blogsData?: IBlog[]
+  blogs?: IBlog[]
   onClickFeatured?: (path: string) => void
   onClickCard?: (path: string) => void
 }
 
 export const Featured = (props: IFeaturedProps) => {
-  const { featuredBlog, blogsData } = props;
+  const { featuredBlog, blogs } = props;
   return (
     <div>
-
       <div className="blog-container">
         <div className="top-section">
           <div className="left-section">
-            <FeatureDetails
-              onClick={() => props?.onClickFeatured && props?.onClickFeatured(`/${props.featuredBlog?.id}`)}
-              published_on={featuredBlog?.published_on}
-              titleText={featuredBlog?.blog_title}
+            <HeroSection
+              heading="Transforming Development & || Operations Together"
+              bgImg={`${BASE_URL}${featuredBlog?.banner_big?.url}`}
+              overlay="#111111"
             />
           </div>
-          <Image className="big-image"
-            mobile_src={featuredBlog?.mobile_banner?.url}
-            desktop_src={featuredBlog?.banner_big?.url}
-          />
         </div>
       </div>
       <div className="blog-cards">
-        {blogsData?.map((blog, index) => <BlogCard onClickCard={props.onClickCard} key={index} blogData={blog} />)}
+        {blogs?.map((item: any, i: number) => {
+          return (
+            !item?.is_featured && (
+              <div key={i}>
+                <BlogCard onClickCard={null as any} blogData={item} />
+              </div>
+            )
+          );
+        })}
       </div>
     </div>
   );
 };
-
-const FeatureDetails = ({
-  titleText,
-  published_on,
-  onClick
-}: {
-  titleText?: string;
-  published_on?: string;
-  onClick: () => void;
-}) => {
-  return (
-    <div>
-      <div className="title-text">
-        <h1 className="title-h1">{titleText}</h1>
-      </div>
-      <PublishDate date={new Date(published_on as any) as any} />
-      <ReadMoreButton onClick={onClick} /> {" "}
-    </div>
-  );
-};
-
-const PublishDate = ({ date }: { date?: string }) => {
-  return (
-    <div className="publish-date">
-      <h4 className="date"> {formatDate(date)} </h4>
-    </div>
-  );
-};
-
-const ReadMoreButton = ({ onClick }: { onClick: () => void }) => {
-  return (
-    <div className="read-more-button">
-      <div onClick={onClick} className="read-blog">
-        Read Blog <NextIcon />
-      </div>
-    </div>
-  );
-};
-
-const NextIcon = () => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width="24"
-    height="24"
-    viewBox="0 0 24 24"
-  >
-    <path
-      fill="currentColor"
-      d="M9.4 18L8 16.6l4.6-4.6L8 7.4L9.4 6l6 6l-6 6Z"
-    />
-  </svg>
-);
