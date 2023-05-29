bf3abac3234616a900563f784a147a3d305b6d56
Jyoti Kamble <jyoti.kamble@coditas.com>
2023-05-29 14:48:36 +0530

    Updation: css fixes for detailed view page of blogs

--- src/components/Featured/index.tsx
+++ src/components/Featured/index.tsx
@@ -1,43 +1,45 @@
-import React from "react";
-import { IBlog } from "../../interfaces";
-import { BlogCard } from "../reusableComponents/BlogCard";
-import "./featured.scss";
-import { HeroSection } from "coditas-ui";
-import { BASE_URL } from "../../HTTP";
+import React from 'react'
+import { IBlog } from '../../interfaces'
+import { BlogCard } from '../reusableComponents/BlogCard'
+import './featured.scss'
+import { HeroSection } from 'coditas-ui'
+import { BASE_URL } from '../../HTTP'
 
 interface IFeaturedProps {
-  featuredBlog?: IBlog;
+  featuredBlog?: IBlog
   blogs?: IBlog[]
   onClickFeatured?: (path: string) => void
   onClickCard?: (path: string) => void
 }
 
 export const Featured = (props: IFeaturedProps) => {
-  const { featuredBlog, blogs } = props;
+  const { featuredBlog, blogs } = props
   return (
     <div>
-      <div className="blog-container">
-        <div className="top-section">
-          <div className="left-section">
+      <div className='blog-container'>
+        <div className='top-section'>
+          <div className='left-section'>
             <HeroSection
-              heading="Transforming Development & || Operations Together"
+              heading={
+                (featuredBlog?.blog_title && featuredBlog?.blog_title) || ''
+              }
               bgImg={`${BASE_URL}${featuredBlog?.banner_big?.url}`}
-              overlay="#111111"
+              overlay='#111111'
             />
           </div>
         </div>
       </div>
-      <div className="blog-cards">
+      <div className='blog-cards'>
         {blogs?.map((item: any, i: number) => {
           return (
             !item?.is_featured && (
               <div key={i}>
-                <BlogCard onClickCard={null as any} blogData={item} />
+                <BlogCard onClickCard={() => {}} blogData={item} />
               </div>
             )
-          );
+          )
         })}
       </div>
     </div>
-  );
-};
+  )
+}
--- src/components/reusableComponents/BlogCard/index.tsx
+++ src/components/reusableComponents/BlogCard/index.tsx
@@ -1,40 +1,44 @@
-import React from "react";
-import { IBlog } from "../../../interfaces";
-import { Image } from "../Image";
-import "./blogCard.scss";
+import React from 'react'
+import { IBlog } from '../../../interfaces'
+import { Image } from '../Image'
+import './blogCard.scss'
 
 interface IBlogCardProps {
-  blogData: IBlog;
+  blogData: IBlog
   onClickCard: (path: string) => void
 }
 
 export const BlogCard = (props: IBlogCardProps) => {
   return (
-    <div className="card-container">
-      <div className="card-image">
+    <div className='card-container'>
+      <div className='card-image'>
         <Image
           desktop_src={props.blogData?.banner_small?.url}
           mobile_src={props.blogData?.mobile_card?.url}
         />
-        <span className="env">Devops</span>
+        <span className='env'>{props?.blogData?.env}</span>
       </div>
-      <div className="bottom-content">
-        <div className="blog-title">{props?.blogData?.blog_title}</div>
-        <div className="line" />
-        <div className="r-font card-sub">{props.blogData?.card_subtitle}</div>
+      <div className='bottom-content'>
+        <div className='blog-title'>{props?.blogData?.blog_title}</div>
+        <div className='line' />
+        <div className='r-font card-sub'>{props.blogData?.card_subtitle}</div>
         <div
-          className="more-button r-font"
-          onClick={() => props?.onClickCard && props?.onClickCard(props?.blogData?.id as any)}
+          className='more-button r-font'
+          onClick={() =>
+            props?.onClickCard && props?.onClickCard(props?.blogData?.id as any)
+          }
         >
           Read more
         </div>
         <span
-          className="mob-read-more"
-          onClick={() => props?.onClickCard && props?.onClickCard(props?.blogData?.id as any)}
+          className='mob-read-more'
+          onClick={() =>
+            props?.onClickCard && props?.onClickCard(props?.blogData?.id as any)
+          }
         >
           Read more
         </span>
       </div>
     </div>
-  );
-};
+  )
+}
